70e42d7b5eee431b83c0c89ac89affc1c91fe791
YXGuan <55643200+YXGuan@users.noreply.github.com>
2021-04-06 20:55:39 -0400

    ok 4.6

---
+++ src/main/java/chatbot/chatbot_invrs/App.java
@@ -0,0 +1,13 @@
+package chatbot.chatbot_invrs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
